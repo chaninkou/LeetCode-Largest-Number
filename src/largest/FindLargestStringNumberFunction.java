a83ec1e7c8786db1c8c52b4b0b0a19231482cf34
chaninkou <michaelkoulove@yahoo.com>
2020-03-04 00:16:09 -0800

    Good question on using compareTo in sort and StringBuilder to store.

--- src/largest/FindLargestStringNumberFunction.java
+++ src/largest/FindLargestStringNumberFunction.java
@@ -1,6 +1,7 @@
 package largest;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class FindLargestStringNumberFunction {
     public String largestNumber(int[] nums) {
@@ -8,16 +9,31 @@ public class FindLargestStringNumberFunction {
             return "";
         }
         
+        // Make it into a string array first
         String[] holder = new String[nums.length];
         
+        // This is the not Lambda version of (s1,s2) -> (s2 + s1).compareTo(s1 + s2))
+        Comparator<String> comp = new Comparator<String>(){
+			@Override
+			public int compare(String o1, String o2) {
+				String first = o1 + o2;
+				String second = o2 + o1;
+				return second.compareTo(first);
+			}
+        };
+        
+        // store the integer as string
         for(int i = 0; i < nums.length; i++){
             holder[i] = String.valueOf(nums[i]);
         }
         
+        // Sort by comparing them
         Arrays.sort(holder, (s1,s2) -> (s2 + s1).compareTo(s1 + s2));
         
-        if(holder[0].charAt(0) == '0')
-            return "0";
+        // if {0,0} should return 0 only, not "00"
+        if(holder[0].charAt(0) == '0'){
+        	return "0";
+        }
         
         StringBuilder sb = new StringBuilder();
         
--- src/largest/Main.java
+++ src/largest/Main.java
@@ -1,7 +1,16 @@
 package largest;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
+		int[] nums = {3,99,34,58,6,9};
+		
+		System.out.println("Input: " + Arrays.toString(nums));
+		
+		FindLargestStringNumberFunction solution = new FindLargestStringNumberFunction();
+		
+		System.out.println("Solution: " + solution.largestNumber(nums));
 		 
 	}
 }
